1db803c0d3845774660a3246bc553440d5220a7c
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-05-17 11:38:12 +0300

    update headers

--- webpage2html.py
+++ webpage2html.py
@@ -67,7 +67,7 @@ def get(index, relpath=None, verbose=True, usecache=True, verify=True, ignore_er
                     log('[ CACHE HIT ] - %s' % full_path)
                 return webpage2html_cache[full_path], None
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0'
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
         }
 
         auth = None
